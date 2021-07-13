a36cf52c35f405437241656bdd37ee80007f2590
Nehal <nehal.naeem2@gmail.com>
2021-07-13 00:10:33 -0400

    added map tiles to show user location to data selfie project

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-app.listen(3000, () => {
-    console.log("listening at 3000")
+app.listen(3001, () => {
+    console.log("listening at 3001")
 });
 app.use(express.static('public'));
 
