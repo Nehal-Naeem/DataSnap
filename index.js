8352766431b7f0598d8e56559a8017222a9c8667
Nehal <nehal.naeem2@gmail.com>
2021-07-21 15:31:42 -0400

    pre DB

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ app.listen(port, () => {
     console.log("listening at port: " + port);
 });
 app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
 
+// Post request routing
+app.post('/location', (request, response) => {
+    console.log('LOL incoming request');
+    let location = request.body;
+    console.log(location);
+    response.json({
+        status: 'success',
+        latitude: location.lat,
+        longitude: location.lon
+    });
+})
 
 
